dd453f52fc7c7b0ab15215b8c52446c4a6bb31e3
strivegfx <notifydevon@gmail.com>
2016-08-20 14:17:23 +1200

    Added in Color dependancy to use with Radium

--- src/widget/styles.js
+++ src/widget/styles.js
@@ -1,10 +1,13 @@
+const color = require('color');
 const {colors, spacing} = require('../variables/all');
+const {black, white} = colors;
+const shadow = color(black).alpha(0.5).rgbString();
 
 module.exports = {
 
     base: {
-        background: colors.white,
-        boxShadow: `0px 0px 10px 0px ${colors.black}`,
+        background: white,
+        boxShadow: `0px 0px 10px 0px ${shadow}`,
         padding: spacing.large
     }
 
